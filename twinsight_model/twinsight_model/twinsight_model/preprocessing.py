eb8707ca0c225b0005b58ee7e43c9b208bb6bb1a
Lakshmi2819 <137554407+Lakshmi2819@users.noreply.github.com>
2025-05-19 10:30:22 -0500

    Delete twinsight_model/twinsight_model/twinsight_model directory

--- twinsight_model/twinsight_model/twinsight_model/preprocessing.py
+++
@@ -1,8 +0,0 @@
-# twinsight_model/preprocessing.py
-
-from sklearn.model_selection import train_test_split
-
-def preprocess_data(df, target_column):
-    X = df.drop(columns=[target_column])
-    y = df[target_column]
-    return train_test_split(X, y, test_size=0.2, random_state=42)
