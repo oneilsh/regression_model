cb1c67d4e3d69739a77b714eff14ac4e902d0591
Lakshmi2819 <137554407+Lakshmi2819@users.noreply.github.com>
2025-05-20 11:25:56 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='twinsight_model',
     version='0.1.0',
+    author='Lakshmi Anandan',
+    author_email='lakshmi19anandan@gmail.com',
+    description='A predictive modeling package for personalized health risk estimation',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Lakshmi2819/Regression-Model',
     packages=find_packages(),
     install_requires=[
         'pandas',
@@ -14,4 +20,10 @@ setup(
             'twinsight-cli = cli:main'
         ]
     },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
 )
